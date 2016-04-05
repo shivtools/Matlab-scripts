67ff6ce738f20260aadb4a441e4563c91096a4fb
Toolsidass <st5wr@ljh206175.richmond.edu>
2016-04-04 21:27:14 -0400

    final version

--- Homework(5:4)/SimpsonPoints.m
+++ Homework(5:4)/SimpsonPoints.m
@@ -1,15 +1,16 @@
 function I = SimpsonPoints(x,y)
 
-	n = length(x)
+	n = length(x);
 	num_intervals = n-1;
 	h = x(2)-x(1);
+    I = 0;
 
 	% Do integration by Simpson's 3/8 method if num_intervals%3 = 0
 	if mod(num_intervals, 3) == 0
 		for i = 1:n
-			if i == 1 | i == n
+			if i == 1 || i == n
 				I = I + y(i);
-			else if mod(i,3) == 1
+            elseif mod(i,3) == 1
 					I = I + 2*y(i);
 			else
 				I = I + 3*y(i);
@@ -21,12 +22,12 @@ function I = SimpsonPoints(x,y)
 	%integration in the first interval is done by the trapezoidal method and the 
 	%rest of the intervals are done with composite Simpson's 3/8 method.
 
-	else if mod(num_intervals, 3) == 1
+    elseif mod(num_intervals, 3) == 1
 		I_trapezoidal = (y(1)+y(2))/2;
 		for	i = 2:n
-			if ((i == 2) | (i == n))
+			if ((i == 2) || (i == n))
 				I = I + y(i);
-			else if mod(i,3) == 2
+            elseif mod(i,3) == 2
 				I = I + 2*y(i);
 			else
 				I = I + 3*y(i);
@@ -39,9 +40,9 @@ function I = SimpsonPoints(x,y)
 	else
 		I_simpson = ((x(2)-x(1))*(y(1)+4*y(2)+y(3)))/3;
 		for i = 3:n
-			if (i == 3) | (i == n)
+			if (i == 3) || (i == n)
 				I = I + y(i);
-			else if mod(i,3) == 0
+            elseif mod(i,3) == 0
 				I = I + 2*y(i);
 			else 
 				I = I + 3*y(i);
