104038ab532509e30e886bb7715f7da63fcaf2d7
Shiv Toolsidass <shivtoolsidass@gmail.com>
2016-04-04 20:43:18 -0400

    latest

--- Homework(5:4)/SimpsonPoints.m
+++ Homework(5:4)/SimpsonPoints.m
@@ -1,12 +1,55 @@
-function I = FirstDerivUnique(x,y)
-% Author: Shiv Toolsidass
+function I = SimpsonPoints(x,y)
 
-numIntervals = length(x)-1;
+	n = length(x)
+	num_intervals = n-1;
+	h = x(2)-x(1);
 
-%If number of intervals in the data points is divisible by 3,do integration
-%by composite 
-if(mod(numIntervals,3) == 0)
-    
-    
+	% Do integration by Simpson's 3/8 method if num_intervals%3 = 0
+	if mod(num_intervals, 3) == 0
+		for i = 1:n
+			if i == 1 | i == n
+				I = I + y(i);
+			else if mod(i,3) == 1
+					I = I + 2*y(i);
+			else
+				I = I + 3*y(i);
+			end
+		end
+		I = I*3*h/8;
+
+	%If number of intervals in data points is one more than a number divisible by 3, 
+	%integration in the first interval is done by the trapezoidal method and the 
+	%rest of the intervals are done with composite Simpson's 3/8 method.
+
+	else if mod(num_intervals, 3) == 1
+		I_trapezoidal = (y(1)+y(2))/2;
+		for	i = 2:n
+			if ((i == 2) | (i == n))
+				I = I + y(i);
+			else if mod(i,3) == 2
+				I = I + 2*y(i);
+			else
+				I = I + 3*y(i);
+			end
+		end
+		I = I*3*h/8 + I_trapezoidal;
+
+	%else the integration over the first two intervals is done with Simpson's 1/3 method
+	%and integration over the rest of the intervals is done by composite Simpson's 3/8 method
+	else
+		I_simpson = ((x(2)-x(1))*(y(1)+4*y(2)+y(3)))/3;
+		for i = 3:n
+			if (i == 3) | (i == n)
+				I = I + y(i);
+			else if mod(i,3) == 0
+				I = I + 2*y(i);
+			else 
+				I = I + 3*y(i);
+			end
+		end
+		I = (I*3*h/8) + I_simpson;
+
+	end
+
 end
-end
+
